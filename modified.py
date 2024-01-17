6f6c61ddef052e66eaea07bcc7348ad53aa78e15
Hitman-vs <mane.vishal11@gmail.com>
2024-01-17 15:52:09 +0530

    deleted old file and create new file it is just like raname file

---
+++ modified.py
@@ -0,0 +1,9 @@
+def vs ():
+    print ("hellow this is my first git projects")
+    print (" enter the valid resoans for the your excuses in this projects")
+vs()
+
+def Ram ():
+    print ("This is your modified some task like your need")
+    print ("you want to do practice more and more for learing this")
+vs()
